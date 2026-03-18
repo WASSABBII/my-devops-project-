1086e5b6e27cdb35c3bc973f89c1e4e13842a345
Nurgali <nurgali.anarbek04@mail.ru>
2026-03-18 12:24:18 +0500

    я добавил проверка

--- app/parser.py
+++ app/parser.py
@@ -1,6 +1,7 @@
 import time
 import redis
 import os
+print("Проверка авто-деплоя") 
 print(f"DEBUG: Мой секретный пароль из переменной: {os.getenv('DB_PASSWORD')}")
 print("Пробую подключиться к Redis...")
 # 'my-db' — это имя сервиса из твоего docker-compose.yml
